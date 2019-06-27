164661b64e134f79fc8bb720865fa0361ae5503f
Erik H <erikjh_24@yahoo.com>
2019-06-26 21:49:14 -0700

    Cleaned up code. Fixed issue where background image wasn't displaying when loading the page from github pages.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,25 +11,21 @@ function countDown(secs, elem) {
     secs--;
     var timer = setTimeout('countDown(' + secs + ',"' + elem + '")', 1000);
 }
-
-
-// BEGIN GAME WHEN 'START' IS CLICKED - Removed as linked to a new HTML document in index.html
-// $("#start-button").on("click", function () {
-
 // KEEPING TRACK OF USERS RIGHT OR WRONG GUESSES
 
 var correctGuess = 0;
 var wrongGuess = 0;
 var noGuess = 0;
 
-$(document).ready(function() {
+
+$(document).ready(function () {
     $("#jumbotron").show();
     $("#jumbotron2").hide();
     $("#jumbotron3").hide();
 });
 
 
-
+// BEGIN GAME WHEN 'START' IS CLICKED 
 $("#start-button").click(function () {
     $("#jumbotron").hide();
     $("#jumbotron2").show();
@@ -37,12 +33,14 @@ $("#start-button").click(function () {
 
 });
 
-$("#submit-button").click(function() {
+// SUBMIT ANSWERS TO QUESTIONS WHEN 'SUBMIT' IS CLICKED
+$("#submit-button").click(function () {
     $("#jumbotron").hide();
     $("#jumbotron2").hide();
     $("#jumbotron3").show();
 });
 
+// THE TRIVIA QUESTIONS
 var questionArr = [
     "1. The Last of Us released for Playstation 3 in this year and went on to win multiple Game of the Year Awards",
     "2. What is Mega Man known as in Japan?",
@@ -52,6 +50,8 @@ var questionArr = [
     "6. What button combination gives you 30 lives in Contra on the Nintendo Entertainment System?"
 ];
 
+
+// THE ANSWERS TO THE TRIVIA QUESTIONS
 var q1Answers = [
     "2012",
     "2013",
@@ -94,61 +94,70 @@ var q6Answers = [
     "&uarr; &uarr; &darr; &darr; &larr; &rarr; &larr; &rarr; B, A, Start" // This is the answer
 ]
 
-// QUESTION 1
+// MAKING QUESTION 1 SHOW ON PAGE
 $('#q1').append('<p>' + questionArr[0] + '</p>');
 
-// QUESTION 1 ANSWERS
+// LISTING THE ANSWER OPTIONS FOR QUESTION 1
 $('#q1a1').append('<p>' + q1Answers[0] + '</p>');
 $('#q1a2').append('<p>' + q1Answers[1] + '</p>');
 $('#q1a3').append('<p>' + q1Answers[2] + '</p>');
 
 
-// QUESTION 2
+// MAKING QUESTION 2 SHOW ON PAGE
 $('#q2').append('<p>' + questionArr[1] + '</p>');
 
-// QUESTION 2 ANSWERS
+// LISTING THE ANSWER OPTIONS FOR QUESTION 2
 $('#q2a1').append('<p>' + q2Answers[0] + '</p>');
 $('#q2a2').append('<p>' + q2Answers[1] + '</p>');
 $('#q2a3').append('<p>' + q2Answers[2] + '</p>');
 
 
-// QUESTION 3
+// MAKING QUESTION 3 SHOW ON PAGE
 $('#q3').append('<p>' + questionArr[2] + '</p>');
 
-// QUESTION 3 ANSWERS
+// LISTING THE ANSWER OPTIONS FOR QUESTION 3
 $('#q3a1').append('<p>' + q3Answers[0] + '</p>');
 $('#q3a2').append('<p>' + q3Answers[1] + '</p>');
 $('#q3a3').append('<p>' + q3Answers[2] + '</p>');
 
 
-// QUESTION 4
+// MAKING QUESTION 4 SHOW ON PAGE
 $('#q4').append('<p>' + questionArr[3] + '</p>');
 
-// QUESTION 4 ANSWERS
+// LISTING THE ANSWER OPTIONS FOR QUESTION 4 
 $('#q4a1').append('<p>' + q4Answers[0] + '</p>');
 $('#q4a2').append('<p>' + q4Answers[1] + '</p>');
 $('#q4a3').append('<p>' + q4Answers[2] + '</p>');
 
 
-// QUESTION 5
+// MAKING QUESTION 5 SHOW ON PAGE
 $('#q5').append('<p>' + questionArr[4] + '</p>');
 
-// QUESTION 5 ANSWERS
+// LISTING THE ANSWER OPTIONS FOR QUESTION 5
 $('#q5a1').append('<p>' + q5Answers[0] + '</p>');
 $('#q5a2').append('<p>' + q5Answers[1] + '</p>');
 $('#q5a3').append('<p>' + q5Answers[2] + '</p>');
 
 
-// QUESTION 6
+// MAKING QUESTION 6 SHOW ON PAGE
 $('#q6').append('<p>' + questionArr[5] + '</p>');
 
-// QUESTION 6 ANSWERS
+// LISTING THE ANSWER OPTIONS FOR QUESTION 6
 $('#q6a1').append('<p>' + q6Answers[0] + '</p>');
 $('#q6a2').append('<p>' + q6Answers[1] + '</p>');
 $('#q6a3').append('<p>' + q6Answers[2] + '</p>');
 
-// DISPLAY RESULTS
+
+
+// DISPLAY RESULTS ON THE PAGE
 $('#rightAnswers').append('<p>' + 'Correct Answers: ' + correctGuess + '</p>');
 $('#wrongAnswers').append('<p>' + 'Wrong Answers: ' + wrongGuess + '</p>');
 $('#noAnswer').append('<p>' + 'No Answer: ' + noGuess + '</p>');
+
+
+
+
+
+
+   
 
